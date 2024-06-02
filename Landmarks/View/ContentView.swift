269cfe85726a0df52e63ba32081c1b209dea8596
Amad Salmon <amad.salmon@icloud.com>
2024-06-02 21:42:15 +0200

    Implement dynamic lists and navigation
    Provide a way for users to see the full list of landmarks, and to view the details about each location.
    Create views that can show information about any landmark.
    Dynamically generate a scrolling list that a user can tap to see a detail view for a landmark.

---
+++ Landmarks/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Salmon, Amad on 2/6/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
