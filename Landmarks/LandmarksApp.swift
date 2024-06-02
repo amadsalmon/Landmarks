2829bf120a3026dffad0d8d4a65c528eeb079a28
Amad Salmon <amad.salmon@icloud.com>
2024-06-02 20:27:40 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Salmon, Amad on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
